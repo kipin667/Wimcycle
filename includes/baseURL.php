42df9c1c1e8ea27f0da01b7c5cbd6bc2ff7d89d0
unknown <xentwork@gmail.com>
2026-08-31 11:34:18 +0700

    delete all folder

--- includes/baseURL.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$preUrl = explode("/", $_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']);
-
-$url = (isset($_SERVER['HTTPS']) ? "https" : "http")."://" . $preUrl[0]."/".$preUrl[1]."/";
-
-define("Base_URL", $url);
-
-echo Base_URL;
